1d42c5c2b01ad8ce60387d3faad354b313f6a7a0
zhanganyang <444234222@qq.com>
2026-05-30 13:42:39 +0800

    Add files via upload

---
+++ data/ren8.js
@@ -0,0 +1,26 @@
+window.XJ_DATA = {
+  "id": "ren8",
+  "username": "15556813075",
+  "password": "123456",
+  "photo": "./loginDa/ren8.jpg",
+  "name": "王学涵",
+  "idNo": "340822200709083209",
+  "gender": "女",
+  "birthday": "2007年09月08日",
+  "school": "上海交通大学",
+  "degree": "本科",
+  "description": "汉语言文学　|　普通全日制",
+  "details": [
+    { "label": "民族", "value": "汉族" },
+    { "label": "证件号码", "value": "340822200709083209" },
+    { "label": "学制", "value": "4年" },
+    { "label": "学历类别", "value": "普通高等教育" },
+    { "label": "分院", "value": "人文学院" },
+    { "label": "系所", "value": "中文系" },
+    { "label": "班级", "value": "25级汉语言文学1班" },
+    { "label": "学号", "value": "524111916957" },
+    { "label": "入学日期", "value": "2025年09月01日" },
+    { "label": "学籍状态", "value": "在籍(注册学籍)" },
+    { "label": "预计毕业日期", "value": "2029年06月30日" }
+  ]
+};
--- data/users.js
+++ data/users.js
@@ -1,1 +1,1 @@
-window.USER_IDS = ["ren5","ren6","ren7"];
+window.USER_IDS = ["ren5","ren6","ren7","ren8"];
