c44263c4fa0e2cdebd732c0546dd6f958de6dcc7
zhanganyang <444234222@qq.com>
2026-06-12 09:15:21 +0800

    Add files via upload

---
+++ data/ren11.js
@@ -0,0 +1,26 @@
+window.XJ_DATA = {
+  "id": "ren11",
+  "username": "19220473160",
+  "password": "123456",
+  "photo": "./loginDa/ren11.jpg",
+  "name": "彭文慧",
+  "idNo": "433130200802162320",
+  "gender": "女",
+  "birthday": "2008年02月16日",
+  "school": "厦门集美大学",
+  "degree": "本科",
+  "description": "电子商务　|　普通全日制",
+  "details": [
+    { "label": "民族", "value": "苗" },
+    { "label": "证件号码", "value": "433130200802162320" },
+    { "label": "学制", "value": "4年" },
+    { "label": "学历类别", "value": "普通高等教育" },
+    { "label": "分院", "value": "工商管理学院" },
+    { "label": "系所", "value": "电子商务系" },
+    { "label": "班级", "value": "25级电子商务3班" },
+    { "label": "学号", "value": "202521215235" },
+    { "label": "入学日期", "value": "2025年09月01日" },
+    { "label": "学籍状态", "value": "在籍(注册学籍)" },
+    { "label": "预计毕业日期", "value": "2029年06月30日" }
+  ]
+};
