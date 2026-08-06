ede1dfd3666e779bb1861f0bdfca75287e293885
zhanganyang <444234222@qq.com>
2026-08-06 13:55:51 +0800

    Add files via upload

---
+++ data/ren12.js
@@ -0,0 +1,26 @@
+window.XJ_DATA = {
+  "id": "ren12",
+  "username": "19280721499",
+  "password": "123456",
+  "photo": "./loginDa/ren12.jpg",
+  "name": "何玉环",
+  "idNo": "522426200612030046",
+  "gender": "女",
+  "birthday": "2006年12月03日",
+  "school": "上海交通大学",
+  "degree": "本科",
+  "description": "视觉设计　|　普通全日制",
+  "details": [
+    { "label": "民族", "value": "汉" },
+    { "label": "证件号码", "value": "522426200612030046" },
+    { "label": "学制", "value": "4年" },
+    { "label": "学历类别", "value": "普通高等教育" },
+    { "label": "分院", "value": "媒体与设计学院" },
+    { "label": "系所", "value": "视觉设计系" },
+    { "label": "班级", "value": "25级视觉传达设计1班" },
+    { "label": "学号", "value": "5240909587" },
+    { "label": "入学日期", "value": "2025年09月01日" },
+    { "label": "学籍状态", "value": "在籍(注册学籍)" },
+    { "label": "预计毕业日期", "value": "2029年06月30日" }
+  ]
+};
--- data/users.js
+++ data/users.js
@@ -1,1 +1,1 @@
-window.USER_IDS = ["ren5","ren6","ren7","ren8","ren11"];
+window.USER_IDS = ["ren5","ren6","ren7","ren8","ren11","ren12"];
